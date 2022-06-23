41c2a01945f0ca6828e2b5e36abedc6346d966eb
Hebert Rocha <70274409+Hebert324@users.noreply.github.com>
2022-06-23 00:27:22 -0300

    Aula-02
    Finalização da aula 01 e 02

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import { useEffect } from "react"
-import { client } from './lib/apollo';
-import { gql } from '@apollo/client';
-
-const GET_LESSONS_QUERY = gql`
-  query {
-  lessons {
-    id
-    title
-  }
-}
-`
+import { Event } from "./pages/Event"
 
 function App() {
-  useEffect(() => {
-    client.query({
-      query: GET_LESSONS_QUERY,
-    }).then(response => {
-      console.log(response.data)
-    })
-  }, [])
-
-  return (
-    <h1 className="text-5xl font-bold text-violet-500" >Hello world</h1>
-  )
+    return(
+      <div>
+        <Event />
+      </div>
+    )
 }
 
 export default App
